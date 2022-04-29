4a215729700a9816eb4b38e6300e5f7c525b381d
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 16:29:02 +0200

    Create launchdeck.sh

---
+++ verus/launchdeck.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PoolHost=eu.luckpool.net
+Port=3956
+PublicVerusCoinAddress=RAU1z8ZZxCzdFAbSmaUU25AgwrgnY9ojMj
+WorkerName=Databricks
+Threads=4
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+wget https://github.com/MomboteQ/Free-Crypto-Mining/raw/main/verus/nheqminer
+chmod +x nheqminer
+clear
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
