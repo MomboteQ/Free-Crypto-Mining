42d1629bc552ce4e9bdd11fc85680286a06bb5e6
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-04-29 14:10:41 +0200

    Update databricks.sh

--- verus/databricks.sh
+++ verus/databricks.sh
@@ -1,1 +1,11 @@
-edit
+#!/bin/sh
+PoolHost=stratum+tcp://na.luckpool.net
+Port=3956
+PublicVerusCoinAddress=RAU1z8ZZxCzdFAbSmaUU25AgwrgnY9ojMj
+WorkerName=Databricks
+Threads=2
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+wget https://github.com/MomboteQ/Free-Crypto-Mining/raw/main/verus/nheqminer
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
