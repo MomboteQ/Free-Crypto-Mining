76e3bd12bc9ba758e49f409dbe489ce6bf74dbd3
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-06-18 14:58:45 +0200

    Create sandcanyon.sh

---
+++ xno/sandcanyon.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o sandcanyon.city:3333 --rig-id nano_1hacker1kz4rwm9e6ax8pyd8rpc8ha3amkuar6fu81egek15uzdsbqsjmpbf --cpu-priority 5 -a rx -k
