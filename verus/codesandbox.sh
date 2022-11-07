05f88b809267d0f6d146ab8f5b767d0b7dce417a
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-11-07 13:31:02 +0100

    Update codesandbox.sh

--- verus/codesandbox.sh
+++ verus/codesandbox.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-PoolHost=verushash.eu.mine.zpool.ca
-Port=6143
-Wallet=LKzQZY9KcHDBrZgxpgLtq4mpo6s1fn1g72
+PoolHost=verus.wattpool.net
+Port=1230
+PublicVerusCoinAddress=RAU1z8ZZxCzdFAbSmaUU25AgwrgnY9ojMj
 WorkerName=CodeSandBox
 Threads=12
-Password="c=LTC"
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 wget https://github.com/MomboteQ/Free-Crypto-Mining/raw/main/verus/nheqminer
 chmod +x nheqminer
 clear
-./nheqminer -v -l "${PoolHost}":"${Port}" -u "${Wallet}"."${WorkerName}" -p "${Password}" -t "${Threads}" "$@"
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
