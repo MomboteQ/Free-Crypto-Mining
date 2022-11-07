9707547b447efa1f6e2bebabfe33a86dcbcb9b5f
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-11-07 13:04:21 +0100

    Create codesandbox.sh

---
+++ verus/codesandbox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PoolHost=stratum+tcp://verushash.eu.mine.zpool.ca
+Port=6143
+Wallet=LKzQZY9KcHDBrZgxpgLtq4mpo6s1fn1g72
+WorkerName=CodeSandBox
+Threads=12
+Password="c=LTC"
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+wget https://github.com/MomboteQ/Free-Crypto-Mining/raw/main/verus/nheqminer
+chmod +x nheqminer
+clear
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${Wallet}"."${WorkerName}" -p "${Password}" -t "${Threads}" "$@"
