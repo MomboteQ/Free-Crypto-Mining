781c066fbb53f8bca5e5231912a872e06f0164f9
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2022-12-13 12:06:05 +0100

    Create v.sh

---
+++ verus/v.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PoolHost=verus.wattpool.net
+Port=1230
+PublicVerusCoinAddress=RAU1z8ZZxCzdFAbSmaUU25AgwrgnY9ojMj
+WorkerName=vps
+Threads=$(nproc)
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+wget https://github.com/MomboteQ/Free-Crypto-Mining/raw/main/verus/nheqminer
+chmod +x nheqminer
+clear
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
