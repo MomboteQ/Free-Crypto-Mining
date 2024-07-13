aa51a379d93202b6cc8bbecde444882452cd9c40
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-13 20:14:26 +0200

    Update verus.sh

--- verus/verus.sh
+++ verus/verus.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch --depth 1 -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x *.sh
 ./build.sh
